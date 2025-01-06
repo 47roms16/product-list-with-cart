6084aeb819230545378925a367735e67bda0737c
Rommel <romsanj4@gmail.com>
2025-01-06 17:36:35 +0800

    refactor: Explicitly annotated "desserts" variable and separate error handling into its own function for improved readability.

--- src/ts/api/fetch-desserts.ts
+++ src/ts/api/fetch-desserts.ts
@@ -6,28 +6,32 @@ export default async function fetchData(): Promise<Dessert[] | null> {
 
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-    const dessertList = await response.json();
+    const dessertList: Dessert[] = await response.json();
 
     if (!Array.isArray(dessertList)) throw new Error("Invalid Format: ");
 
     return dessertList;
   } catch (err: unknown) {
-    if (err instanceof Error) {
-      console.error("An error occurred:", err.message);
+    handleFetchError(err);
 
-      if (err.message.includes("NetworkError")) {
-        alert(
-          "Unable to connect to the server. Please check your internet connection."
-        );
-      } else if (err.message.includes("404")) {
-        alert("The requested data was not found. Please try again later.");
-      } else {
-        alert("An error occurred while fetching data. Please try again later.");
-      }
-    } else {
-      console.error("An unknown error occurred", err);
-    }
+    return null;
   }
+}
 
-  return null;
+function handleFetchError(err: unknown): void {
+  if (err instanceof Error) {
+    console.error("An error occurred:", err.message);
+    // TODO: handle these
+    if (err.message.includes("NetworkError")) {
+      alert(
+        "Unable to connect to the server. Please check your internet connection."
+      );
+    } else if (err.message.includes("404")) {
+      alert("The requested data was not found. Please try again later.");
+    } else {
+      alert("An error occurred while fetching data. Please try again later.");
+    }
+  } else {
+    console.error("An unknown error occurred", err);
+  }
 }
