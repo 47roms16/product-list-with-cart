bebb63f060e9fa980c9a69c3b9a0625dde4d50cb
Rommel <romsanj4@gmail.com>
2025-01-14 13:11:07 +0800

    feat: add `updateCartArray` function to manage cart and UI updates
    - Introduced `updateCartArray` to handle cart item removal and UI updates
    - Added relevant imports for `findMatchingId`, `updateCartUi`, and `restoreAddToCartBtn`
    - Enhanced cart handling logic to remove items with zero quantity and update UI accordingly

--- src/ts/ui/components/cart/cart.ts
+++ src/ts/ui/components/cart/cart.ts
@@ -1,3 +1,19 @@
 import { cartItem } from "./types/cart-item";
 
-export const cartProducts: cartItem[] = [];
+import updateCartUi from "./handlers/update-ui";
+import { restoreAddToCartBtn } from "../products/helpers/update-button";
+
+import findMatchingId from "../../../utils/find-matching-id";
+
+export let cartProducts: cartItem[] = [];
+
+export function updateCartArray(productId: number): void {
+  const matchingProduct = findMatchingId(productId);
+
+  if (matchingProduct?.quantity === 0) {
+    cartProducts = cartProducts.filter((product) => product.productId !== productId);
+  }
+
+  updateCartUi();
+  restoreAddToCartBtn(productId);
+}
