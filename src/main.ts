12c70d6779d0813f5383d94c6092a3225ce02aa3
Rommel <romsanj4@gmail.com>
2025-01-14 13:09:12 +0800

    refactor: rename `display-desserts.ts` to `init-ui.ts`
    - Renamed file to reflect its broader purpose of initializing both dessert and cart UI
    - Improved clarity of file’s role in the application
    
    refactor: move `DOMContentLoaded` listener and handler to `main.ts`
    
    - Moved event listener for `DOMContentLoaded` and its associated handler (`initUi`) to `main.ts` for centralized initialization logic
    - Improved structure by isolating the initial app setup in one place

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import "../src/styles/style.css";
-import "../src/styles/components/main-content.css";
-import "../src/styles/components/grid-items.css";
-import "../src/styles/components/cart.css";
+import "./styles/style.css";
+import "./styles/components/main.css";
+import "./styles/components/cart.css";
 
 import "./ts/api/fetch-desserts";
-import "./ts/ui/display-desserts";
+import initUi from "./ts/ui/components/products/init-ui";
+
+document.addEventListener("DOMContentLoaded", initUi);
---
+++ src/ts/ui/components/products/init-ui.ts
@@ -0,0 +1,33 @@
+import { products } from "../../../api/products";
+
+import updateCartUi from "../cart/handlers/update-ui";
+import setUpAddToCartBtns from "../handlers/add-to-cart-listeners";
+import generateMenuItem from "./generate-dessert-html";
+
+import "./grid-items.css";
+
+export default async function initUi(): Promise<void> {
+  try {
+    const menuSection = document.querySelector(".menu__wrap") as HTMLElement;
+    const menuWrap = document.createElement("ul");
+    menuWrap.className = "menu";
+
+    menuSection.appendChild(menuWrap);
+
+    const desserts = await products;
+
+    if (!desserts) {
+      return;
+    }
+    const menuItems: HTMLLIElement[] = desserts.map((dessert) => {
+      return generateMenuItem(dessert);
+    });
+
+    menuItems.forEach((item) => menuWrap.appendChild(item));
+
+    setUpAddToCartBtns(menuWrap);
+    updateCartUi();
+  } catch (err) {
+    console.error(err);
+  }
+}
