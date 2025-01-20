cb1eedcaef1d118911f7ab707e83af69844fbf59
Rommel <romsanj4@gmail.com>
2025-01-20 15:58:35 +0800

    refactor: Move menu items rendering to a reusable function.

---
+++ src/ts/ui/components/products/helpers/render-menu-items.ts
@@ -0,0 +1,13 @@
+import { Dessert } from "../../../types/dessert-structure";
+import populateUlist from "../../utils/populate-ulist";
+import generateMenuItem from "../generate-dessert-html";
+
+export default async function renderMenuUi(desserts: Dessert[]): Promise<void> {
+  const menuSection = document.querySelector(".menu__wrap") as HTMLElement;
+  menuSection.textContent = "";
+
+  const { uListElem } = populateUlist(desserts, generateMenuItem);
+  uListElem.className = "menu";
+
+  menuSection.appendChild(uListElem);
+}
--- src/ts/ui/components/products/init-ui.ts
+++ src/ts/ui/components/products/init-ui.ts
@@ -2,32 +2,18 @@ import { products } from "../../../api/products";
 
 import updateCartUi from "../cart/handlers/update-ui";
 import setUpAddToCartBtns from "../handlers/add-to-cart-listeners";
-import generateMenuItem from "./generate-dessert-html";
 
 import "./grid-items.css";
+import renderMenuUi from "./helpers/render-menu-items";
 
 export default async function initUi(): Promise<void> {
   try {
-    const mainSection = document.getElementById("main") as HTMLElement;
-    const menuSection = document.querySelector(".menu__wrap") as HTMLElement;
-    const menuWrap = document.createElement("ul");
-
-    menuWrap.className = "menu";
-
-    menuSection.appendChild(menuWrap);
-
     const desserts = await products;
 
-    if (!desserts) {
-      return;
-    }
-    const menuItems: HTMLLIElement[] = desserts.map((dessert) => {
-      return generateMenuItem(dessert);
-    });
+    if (!desserts) return;
 
-    menuItems.forEach((item) => menuWrap.appendChild(item));
-
-    setUpAddToCartBtns(mainSection);
+    renderMenuUi(desserts);
+    setUpAddToCartBtns(desserts);
     updateCartUi();
   } catch (err) {
     console.error(err);
